b3de6b38206b4daf4c9c369d61975e3d67cd597a
tarek.fouda <tarek.fouda@roaa.tech>
2023-11-06 17:55:35 +0200

    kjsdn

--- example/js/hello.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Description: module example
- * @Author: ekibun
- * @Date: 2020-10-03 00:29:45
- * @LastEditors: ekibun
- * @LastEditTime: 2020-10-03 00:32:37
- */
-export default (name) => `hello ${name}!`;
